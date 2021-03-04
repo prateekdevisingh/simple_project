48a43858df3abc213a8c087bb5a98f3374cb7eaf
Prateek Chaudhary <Prateek.Chaudhary@XEBIAINDIA.LOCAL>
2021-03-04 16:52:32 +0530

    just checking script

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -3,9 +3,9 @@ echo 'Starting to Deploy...'
 ssh ubuntu@18.216.139.184
 DIR="/root/test_dir"
 if [ -d "$DIR" ]; then
-  echo "dir present"
+  echo 'dir present"'
 else
-  echo "making new dir"
+  echo 'making new dir'
   mkdir $DIR
 fi
 echo 'Deployment completed successfully'
