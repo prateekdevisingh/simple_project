4e96572a58c7c14251beea2a6747642a972fa2e0
Prateek Chaudhary <Prateek.Chaudhary@XEBIAINDIA.LOCAL>
2021-03-05 13:06:43 +0530

    adding new command to ssh

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 echo 'Starting to Deploy...'
-ssh ubuntu@3.139.90.157 "sudo mkdir -p test"
+ssh -v -i testawskey.pem ubuntu@3.139.90.157 "sudo mkdir -p demo"
 echo 'Deployment completed successfully'
