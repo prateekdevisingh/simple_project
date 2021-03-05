5d145bb4dcde858beffaadd5cdb437c42314048d
Prateek Chaudhary <Prateek.Chaudhary@XEBIAINDIA.LOCAL>
2021-03-05 15:15:26 +0530

    new

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 echo 'Starting to Deploy...'
-ssh -i id_rsa ubuntu@3.140.252.102 "sudo mkdir -p demo"
+ssh ubuntu@3.140.252.102 "sudo mkdir -p demo"
 echo 'Deployment completed successfully'
