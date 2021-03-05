311b70a1366de6eb1e44d523792c01b063216cb3
Prateek Chaudhary <Prateek.Chaudhary@XEBIAINDIA.LOCAL>
2021-03-05 14:45:51 +0530

    adding ew ip and id_rsa

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 echo 'Starting to Deploy...'
-ssh -v -i testawskey.pem ubuntu@3.139.90.157 "sudo mkdir -p demo"
+ssh -i id_rsa ubuntu@3.140.252.102 "sudo mkdir -p demo"
 echo 'Deployment completed successfully'
